caceb0f9a6ea24cbb32798f601b34d981e6d82a8
thecreatorPG3D <55181320+Greenphx9@users.noreply.github.com>
2024-08-12 18:13:19 +1000

    Fix battle text box

--- src/battle_bg.c
+++ src/battle_bg.c
@@ -160,10 +160,10 @@ static const struct WindowTemplate sStandardBattleWindowTemplates[] =
         .bg = 0,
         .tilemapLeft = 1,
         .tilemapTop = 15,
-        .width = 28,
+        .width = 26,
         .height = 4,
         .paletteNum = 0,
-        .baseBlock = 0x090
+        .baseBlock = 0x0090,
     },
     [B_WIN_ACTION_PROMPT] = {
         .bg = 0,
