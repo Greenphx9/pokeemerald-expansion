5163e9c3427a13a3f7cfa00258c45d2d1e3a63fa
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2024-08-29 17:28:48 +1000

    Fix expansion-specific errors

--- src/pokemon_sprite_visualizer.c
+++ src/pokemon_sprite_visualizer.c
@@ -1110,7 +1110,6 @@ void CB2_Pokemon_Sprite_Visualizer(void)
             AllocateMonSpritesGfx();
 
             LoadPalette(sBgColor, 0, 2);
-            LoadMonIconPalette(SPECIES_BULBASAUR);
 
             SetGpuReg(REG_OFFSET_DISPCNT, DISPCNT_OBJ_ON | DISPCNT_OBJ_1D_MAP);
             ShowBg(0);
@@ -1683,7 +1682,6 @@ static void ReloadPokemonSprites(struct PokemonSpriteVisualizer *data)
     FreeMonIconPalettes();
 
     AllocateMonSpritesGfx();
-    LoadMonIconPalettePersonality(species, (data->isFemale ? FEMALE_PERSONALITY : MALE_PERSONALITY));
 
     //Update instructions
     PrintInstructionsOnWindow(data);
