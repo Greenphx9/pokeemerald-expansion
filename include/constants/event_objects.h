a473ad9a7507f9e28e4ad0e6d5e5d6fead48dd41
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2025-01-18 17:50:41 +1100

    FRLG-accurate naming screen

--- include/constants/event_objects.h
+++ include/constants/event_objects.h
@@ -252,11 +252,12 @@
 #define OBJ_EVENT_GFX_GREEN_SURF                 248
 #define OBJ_EVENT_GFX_GREEN_FIELD_MOVE           249
 #define OBJ_EVENT_GFX_GREEN_FISH                 250
+#define OBJ_EVENT_GFX_BLUE                       251
 
 // NOTE: The maximum amount of object events has been expanded from 255 to 65535.
 // Since dynamic graphics ids still require at least 16 free values, the actual limit
 // is 65519, but even considering follower Pokémon, this should be more than enough :)
-#define NUM_OBJ_EVENT_GFX                        251
+#define NUM_OBJ_EVENT_GFX                        252
 
 
 // These are dynamic object gfx ids.
