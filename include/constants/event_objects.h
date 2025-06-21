1e77f646f96ecf239ab44ef07ea2fd7d8282b0ea
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2025-06-21 14:34:07 +1000

    Fix cherry-pick conflict with light sprite ID

--- include/constants/event_objects.h
+++ include/constants/event_objects.h
@@ -249,21 +249,21 @@
 #define OBJ_EVENT_GFX_LIGHT_SPRITE               241
  
 // RG Sprites
-#define OBJ_EVENT_GFX_RED_NORMAL                 241
-#define OBJ_EVENT_GFX_RED_BIKE                   242
-#define OBJ_EVENT_GFX_RED_SURF                   243
-#define OBJ_EVENT_GFX_RED_FIELD_MOVE             244
-#define OBJ_EVENT_GFX_RED_FISH                   245
-#define OBJ_EVENT_GFX_GREEN_NORMAL               246
-#define OBJ_EVENT_GFX_GREEN_BIKE                 247
-#define OBJ_EVENT_GFX_GREEN_SURF                 248
-#define OBJ_EVENT_GFX_GREEN_FIELD_MOVE           249
-#define OBJ_EVENT_GFX_GREEN_FISH                 250
+#define OBJ_EVENT_GFX_RED_NORMAL                 242
+#define OBJ_EVENT_GFX_RED_BIKE                   243
+#define OBJ_EVENT_GFX_RED_SURF                   244
+#define OBJ_EVENT_GFX_RED_FIELD_MOVE             245
+#define OBJ_EVENT_GFX_RED_FISH                   246
+#define OBJ_EVENT_GFX_GREEN_NORMAL               247
+#define OBJ_EVENT_GFX_GREEN_BIKE                 248
+#define OBJ_EVENT_GFX_GREEN_SURF                 249
+#define OBJ_EVENT_GFX_GREEN_FIELD_MOVE           250
+#define OBJ_EVENT_GFX_GREEN_FISH                 251
 
 // NOTE: The maximum amount of object events has been expanded from 255 to 65535.
 // Since dynamic graphics ids still require at least 16 free values, the actual limit
 // is 65519, but even considering follower Pokémon, this should be more than enough :)
-#define NUM_OBJ_EVENT_GFX                        251
+#define NUM_OBJ_EVENT_GFX                        252
 
 
 // These are dynamic object gfx ids.
