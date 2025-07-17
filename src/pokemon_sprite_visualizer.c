f6fce18434f700532396c44c33e7cee6d86cf3bc
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2025-07-17 20:35:52 +1000

    Victini icon

--- src/pokemon_sprite_visualizer.c
+++ src/pokemon_sprite_visualizer.c
@@ -1269,7 +1269,7 @@ void CB2_Pokemon_Sprite_Visualizer(void)
             data = AllocZeroed(sizeof(struct PokemonSpriteVisualizer));
             SetStructPtr(taskId, data);
 
-            data->currentmonId = SPECIES_ARCEUS_GROUND,
+            data->currentmonId = SPECIES_VICTINI,
             species = data->currentmonId;
 
             //Print instructions
